735dcfc0c8e9cce146eaf8a70142a2047de3269c
Fabian Lopez Gonzalez <fabianlopez@pm.me>
2022-09-21 18:32:14 +0200

    chore: server: added temptative router.js info

--- server/router.js
+++ server/router.js
@@ -1,6 +1,13 @@
 const KoaRouter = require('@koa/router');
-const WannaGo = require
+const {
+  getWannaGos,
+  postAwannaGo,
+} = require('./controllers/wannaGoController');
 
 const router = new KoaRouter();
 
+router.get('/wannaGos', getWannaGos)
+router.post('/wannaGo', postAwannaGo)
+
 module.exports = router;
+
