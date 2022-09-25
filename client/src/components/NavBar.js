d466dc25b54be5f160302137d36d060b696985ec
Fabian Lopez Gonzalez <fabianlopez@pm.me>
2022-09-25 17:18:46 +0200

    feat: added logo as a button to landing page
    n

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,27 +3,41 @@ import {
   SignUpButton,
   LogoutButton,
   DashBoardButton,
+  LandingButton,
 } from './NavBarButtons';
 import { useAuth } from '../contexts/AuthContext';
-
+import { useLocation } from 'react-router-dom';
 import '../css/NavBar.css';
 
 const NavBar = () => {
   const { currentUser, logOut } = useAuth();
 
+  const location = useLocation();
   return (
     <nav className='nav-container'>
       <div>
-        {currentUser ? (
-          <>
-            <LogoutButton logOut={logOut} />
-            <DashBoardButton />
-          </>
-        ) : (
+        {currentUser && (
+          <div className='nav-btns-container'>
+            <LandingButton />
+            <div>
+              <LogoutButton logOut={logOut} />
+              <DashBoardButton />
+            </div>
+          </div>
+        )}
+        {!currentUser && location.pathname === '/' ? (
           <>
             <LogInButton />
             <SignUpButton />
           </>
+        ) : (
+          <div className='nav-btns-container'>
+            <LandingButton />
+            <div>
+              <LogoutButton logOut={logOut} />
+              <DashBoardButton />
+            </div>
+          </div>
         )}
       </div>
     </nav>
--- client/src/components/NavBarButtons.js
+++ client/src/components/NavBarButtons.js
@@ -2,29 +2,25 @@ import logoutSoft from '../icons/logout-soft.png';
 import signUpSoft from '../icons/sign-up-soft.png';
 import loginSoft from '../icons/login-soft.png';
 import homeSoft from '../icons/home-icon-soft.png';
+import logo from '../logo.jpg';
 import { Link, useNavigate } from 'react-router-dom';
 
 export const DashBoardButton = () => {
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate('/dashboard');
-  };
   return (
     <img
       className='icons'
       src={homeSoft}
-      onClick={handleClick}
       alt='dashboard icon'
     ></img>
   );
 };
 
-export const LogoutButton = ({logOut}) => {
-  const navigate = useNavigate()
+export const LogoutButton = ({ logOut }) => {
+  const navigate = useNavigate();
   const handleClick = async () => {
-    await logOut()
+    await logOut();
     navigate('/');
-  }
+  };
   return (
     <img
       className='icons'
@@ -36,41 +32,38 @@ export const LogoutButton = ({logOut}) => {
 };
 
 export const SignUpButton = () => {
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate('/signup');
-  };
   return (
-    <img
-      className='icons'
-      src={signUpSoft}
-      onClick={handleClick}
-      alt='sign up icon'
-    />
+    <Link to={'/signup'}>
+      <img
+        className='icons'
+        src={signUpSoft}
+        alt='signup icon'
+      />
+    </Link>
   );
 };
 
 export const LogInButton = () => {
   return (
-    <Link to={'/Login'}>
+    <Link to={'/login'}>
       <img
         className='icons'
         src={loginSoft}
+        alt='login icon'
       ></img>
     </Link>
   );
 };
 
-export const HomeButton = () => {
+export const LandingButton = () => {
   return (
-    <img
-      className='icons'
-      src={homeSoft}
-      onClick={<Link>{'/'}</Link>}
-    ></img>
+    <Link to={'/'}>
+      <img
+        className='icons logo'
+        src={logo}
+        alt='logo'
+      ></img>
+    </Link>
   );
 };
-
-
-
 
