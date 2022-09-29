164e7ef4f88d12d7351a0af87520da8f8ba7fdaa
aferrarifirmo <anaferrarifirmo@gmail.com>
2022-09-29 13:24:22 +0200

    Chenges on Remote A - test

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,5 @@
+// more changeeeeeees here!
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -17,13 +19,7 @@ root.render(
   // </React.StrictMode>
 );
 
-// testing branches merging from experimental_branch_1
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-// test commit number 2
-
