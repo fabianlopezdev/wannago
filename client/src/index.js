06d44d72cba8c6fcfd34247fba7dbb49d02cf098
aferrarifirmo <anaferrarifirmo@gmail.com>
2022-09-29 11:58:49 +0200

    testing branches and merging

--- client/src/index.js
+++ client/src/index.js
@@ -17,6 +17,8 @@ root.render(
   // </React.StrictMode>
 );
 
+// testing branches merging from experimental_branch_1
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
