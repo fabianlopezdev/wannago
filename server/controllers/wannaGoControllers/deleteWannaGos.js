b17fcd836788a9a47fc3deb0cc313099a7ef7c2a
keplib <keplib@gmail.com>
2022-09-29 16:17:52 +0200

    refactor(server folder): remove not used modules, variables, unnecessary newlines and whitespaces
    To start out with legacy project backend has been reviewed and all unnecessary modules, whitespaces,
    console logs have been removed to make the code more readable

--- server/controllers/wannaGoControllers/deleteWannaGos.js
+++ server/controllers/wannaGoControllers/deleteWannaGos.js
@@ -4,15 +4,13 @@ const WannaGo = require('../../models/wannaGoModel');
 const deleteWannaGo = async (ctx) => {
 try {
   const { id } = ctx.request.body;
-  console.log('this is id', id);
   const deletedWannaGo = await WannaGo.findByIdAndRemove(id);
   console.log(`The WannaGo: ${deletedWannaGo} was deleted`)
   ctx.status =201;
   ctx.body = deletedWannaGo
 } catch (e) {
     console.log(`Error in deleteWannaGO function from controllers: ${e}`);
-  
-}
+  }
 }
 
 module.exports = {deleteWannaGo};
